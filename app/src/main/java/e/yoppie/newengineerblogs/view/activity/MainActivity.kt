64ae9da6dd1da1aa124fa0b48277afc56bd107a2
yoshiya12x <hikowaiide@gmail.com>
2019-01-05 17:20:38 +0900

    split by package

--- app/src/main/java/e/yoppie/newengineerblogs/view/activity/MainActivity.kt
+++ app/src/main/java/e/yoppie/newengineerblogs/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package e.yoppie.newengineerblogs
+package e.yoppie.newengineerblogs.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import e.yoppie.newengineerblogs.R
 
 class MainActivity : AppCompatActivity() {
 
