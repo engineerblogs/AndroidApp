be750319011e617d4409da539e8541f423b481f5
yoshiya12x <hikowaiide@gmail.com>
2019-01-08 23:35:59 +0900

    apply articleViewModel

--- app/src/main/java/e/yoppie/newengineerblogs/view/adapter/ArticleRecyclerAdapter.kt
+++ app/src/main/java/e/yoppie/newengineerblogs/view/adapter/ArticleRecyclerAdapter.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import e.yoppie.newengineerblogs.R
 import e.yoppie.newengineerblogs.view.viewHolder.ArticleViewHolder
+import e.yoppie.newengineerblogs.viewmodel.ArticleViewModel
 
 class ArticleRecyclerAdapter(private val articles: ArrayList<String>) : RecyclerView.Adapter<ArticleViewHolder>(){
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding: ViewDataBinding = DataBindingUtil.inflate(layoutInflater, R.layout.article_item, parent, false)
@@ -20,7 +22,7 @@ class ArticleRecyclerAdapter(private val articles: ArrayList<String>) : Recycler
     }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
-        holder.bind(articles[position])
+        holder.bind(ArticleViewModel(articles[position]))
     }
 
 }
--- app/src/main/java/e/yoppie/newengineerblogs/viewmodel/ArticleViewModel.kt
+++ app/src/main/java/e/yoppie/newengineerblogs/viewmodel/ArticleViewModel.kt
@@ -2,9 +2,11 @@ package e.yoppie.newengineerblogs.viewmodel
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.util.Log
+import android.view.View
 
-class ArticleViewModel : ViewModel(){
-    var articleText: MutableLiveData<String> = MutableLiveData()
-
-
+class ArticleViewModel(val articleTitle: String) : ViewModel(){
+    fun onClick(view: View) {
+        Log.d("debug", "on_click_test")
+    }
 }
