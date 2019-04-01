0dcd4bc7faae882f5281cb077a754239cb7b3b68
yoshiya12x <hikowaiide@gmail.com>
2019-04-01 19:57:06 +0900

    fix data class Categories

--- app/src/main/java/e/yoppie/newengineerblogs/model/data/Category.kt
+++ app/src/main/java/e/yoppie/newengineerblogs/model/data/Category.kt
@@ -1,7 +1,11 @@
 package e.yoppie.newengineerblogs.model.data
 
+import com.squareup.moshi.Json
+
 data class Category(
+        @Json(name = "companyName")
         val name: String,
+        @Json(name = "companyId")
         val id: String,
         val articles: MutableList<Article>
 )
