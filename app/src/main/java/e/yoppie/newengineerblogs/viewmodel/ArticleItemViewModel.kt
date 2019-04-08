8ab80cb52c5da7b30365a78793619a0b68d37f75
yoshiya12x <hikowaiide@gmail.com>
2019-04-08 15:51:54 +0900

    edit article tab

--- app/src/main/java/e/yoppie/newengineerblogs/viewmodel/ArticleItemViewModel.kt
+++ app/src/main/java/e/yoppie/newengineerblogs/viewmodel/ArticleItemViewModel.kt
@@ -5,13 +5,22 @@ import android.arch.lifecycle.ViewModel
 import e.yoppie.newengineerblogs.model.data.Article
 
 class ArticleItemViewModel : ViewModel() {
+    val id = MutableLiveData<String>()
     val title = MutableLiveData<String>()
+    val thumbnail = MutableLiveData<String>()
+    val url = MutableLiveData<String>()
+    val publishedDate = MutableLiveData<String>()
     val author = MutableLiveData<String>()
+
 
     private val item = MutableLiveData<Article>().apply {
         this.observeForever {
             it?.apply {
+                this@ArticleItemViewModel.id.postValue(this.id)
                 this@ArticleItemViewModel.title.postValue(this.title)
+                this@ArticleItemViewModel.thumbnail.postValue(this.thumbnail)
+                this@ArticleItemViewModel.url.postValue(this.url)
+                this@ArticleItemViewModel.publishedDate.postValue(this.publishedDate)
                 this@ArticleItemViewModel.author.postValue(this.author)
             }
         }
