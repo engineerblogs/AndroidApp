cbd128a81a2d48c9f5489582d859a68ded984851
yoshiya12x <hikowaiide@gmail.com>
2019-04-08 18:26:51 +0900

    arrange articleactivity ui

--- app/src/main/java/e/yoppie/newengineerblogs/view/activity/ArticleActivity.kt
+++ app/src/main/java/e/yoppie/newengineerblogs/view/activity/ArticleActivity.kt
@@ -1,29 +1,27 @@
 package e.yoppie.newengineerblogs.view.activity
 
-import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
 import android.webkit.WebViewClient
 import e.yoppie.newengineerblogs.R
-import e.yoppie.newengineerblogs.databinding.ActivityArticleBinding
+import kotlinx.android.synthetic.main.activity_article.*
 
 class ArticleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article)
+        setSupportActionBar(articleToolBar)
+        articleToolBar.title = intent.getStringExtra("title")
 
-        val binding = DataBindingUtil.setContentView<ActivityArticleBinding>(this, R.layout.activity_article)
-
-        Log.d("yoshiya_debug", intent.getStringExtra("title"))
-
-        binding.articleWebView.webViewClient = WebViewClient()
-        binding.articleWebView.loadUrl(intent.getStringExtra("url"))
+        articleWebView.webViewClient = WebViewClient()
+        articleWebView.loadUrl(intent.getStringExtra("url"))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.article_menu, menu)
         return true
     }
+
 }
