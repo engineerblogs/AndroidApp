c28c21dd3f8fac888b097d6d67eafc304891fb18
yoshiya12x <hikowaiide@gmail.com>
2019-04-13 12:07:02 +0900

    remove commentout

--- app/src/main/java/e/yoppie/newengineerblogs/view/activity/HomeActivity.kt
+++ app/src/main/java/e/yoppie/newengineerblogs/view/activity/HomeActivity.kt
@@ -1,34 +1,13 @@
 package e.yoppie.newengineerblogs.view.activity
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import e.yoppie.newengineerblogs.R
-import e.yoppie.newengineerblogs.view.fragment.AllArticleFragment
-import e.yoppie.newengineerblogs.view.fragment.BookmarkFragment
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
-
-//    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-//        when (item.itemId) {
-//            R.id.allArticleItem -> {
-//                supportFragmentManager.beginTransaction()
-//                        .replace(R.id.frameLayout, AllArticleFragment())
-//                        .commit()
-//                return@OnNavigationItemSelectedListener true
-//            }
-//            R.id.bookmarkItem -> {
-//                supportFragmentManager.beginTransaction()
-//                        .replace(R.id.frameLayout, BookmarkFragment())
-//                        .commit()
-//                return@OnNavigationItemSelectedListener true
-//            }
-//        }
-//        false
-//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,14 +15,5 @@ class HomeActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.navHostFragment)
         NavigationUI.setupWithNavController(bottomNavigation, navController)
-
-//        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-//
-//        // ここでSelectCompanyActivityの判定する
-//        // データなかったらSelectCompanyActivity
-//        // データあったら以下
-//        supportFragmentManager.beginTransaction()
-//                .replace(R.id.frameLayout, AllArticleFragment())
-//                .commit()
     }
 }
--- app/src/main/java/e/yoppie/newengineerblogs/view/fragment/CategoryFragment.kt
+++ app/src/main/java/e/yoppie/newengineerblogs/view/fragment/CategoryFragment.kt
@@ -7,7 +7,6 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
