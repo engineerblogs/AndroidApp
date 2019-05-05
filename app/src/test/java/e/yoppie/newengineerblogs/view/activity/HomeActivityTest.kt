b76dea5aeff0fca8c37780dd5d1b99b8b32f83c6
yoshiya12x <hikowaiide@gmail.com>
2019-05-05 16:04:42 +0900

    add junit5

---
+++ app/src/test/java/e/yoppie/newengineerblogs/view/activity/HomeActivityTest.kt
@@ -0,0 +1,20 @@
+package e.yoppie.newengineerblogs.view.activity
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class HomeActivityTest {
+
+    @Test
+    fun onCreate() {
+    }
+
+    @Test
+    fun onCreateOptionsMenu() {
+    }
+
+    @Test
+    fun onOptionsItemSelected() {
+    }
+}
