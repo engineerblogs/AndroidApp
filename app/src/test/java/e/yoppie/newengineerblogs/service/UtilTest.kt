208f766f81fcb8a3a3c4f306d10d1490aa908ff2
yoshiya12x <hikowaiide@gmail.com>
2019-05-06 01:07:47 +0900

    add robolectric

---
+++ app/src/test/java/e/yoppie/newengineerblogs/service/UtilTest.kt
@@ -0,0 +1,23 @@
+package e.yoppie.newengineerblogs.service
+
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class UtilTest {
+
+    @Test
+    fun loadImage() {
+    }
+
+    @Test
+    fun clipBoardCopy() {
+
+    }
+
+    @Test
+    fun isNetConnection() {
+    }
+}
