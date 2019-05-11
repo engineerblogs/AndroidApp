3de8a92a089844a27a05f32a041d1b42e91b0190
yoshiya12x <hikowaiide@gmail.com>
2019-05-11 15:16:27 +0900

    create clipBoardCopy test with assertj

--- app/src/test/java/e/yoppie/newengineerblogs/service/UtilTest.kt
+++ app/src/test/java/e/yoppie/newengineerblogs/service/UtilTest.kt
@@ -1,10 +1,14 @@
 package e.yoppie.newengineerblogs.service
 
+import android.content.ClipboardManager
+import android.content.Context
 import org.junit.Test
-import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.assertj.core.api.Assertions.*
 
+@Suppress("DEPRECATION")
 @RunWith(RobolectricTestRunner::class)
 class UtilTest {
 
@@ -14,7 +18,11 @@ class UtilTest {
 
     @Test
     fun clipBoardCopy() {
-
+        val context = RuntimeEnvironment.application
+        Util.clipBoardCopy(context, "clip_test")
+        val clipboardManager: ClipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipText = clipboardManager.text
+        assertThat(clipText).isEqualTo("clip_test")
     }
 
     @Test
