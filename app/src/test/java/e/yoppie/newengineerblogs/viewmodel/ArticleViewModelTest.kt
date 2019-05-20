041234d4c7f73fa35a377154fdc54f9187bcc74a
yoshiya12x <hikowaiide@gmail.com>
2019-05-20 23:46:32 +0900

    add ArticleViewModelTest using RobolectricTestRunner

---
+++ app/src/test/java/e/yoppie/newengineerblogs/viewmodel/ArticleViewModelTest.kt
@@ -0,0 +1,33 @@
+package e.yoppie.newengineerblogs.viewmodel
+
+import e.yoppie.newengineerblogs.model.data.Article
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.assertj.core.api.Assertions.*
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ArticleViewModelTest {
+    private lateinit var articleViewModel: ArticleViewModel
+
+    @Before
+    fun setUp() {
+        articleViewModel = ArticleViewModel()
+    }
+
+    @Test
+    fun set() {
+        val articleList = mutableListOf(
+                Article("0", "title_0", "thumbnail_0", "url_0", "publishedDate_0", "author_0"),
+                Article("1", "title_1", "thumbnail_1", "url_1", "publishedDate_1", "author_1"),
+                Article("2", "title_2", "thumbnail_2", "url_2", "publishedDate_2", "author_2")
+        )
+        articleViewModel.set(articleList)
+        assertThat(articleViewModel.articleListLiveData.value).containsAll(articleList)
+    }
+
+    @Test
+    fun loadArticles() {
+    }
+}
