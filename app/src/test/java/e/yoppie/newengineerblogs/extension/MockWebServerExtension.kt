be81d61087fab30cf3831000444d76501cbf97ab
yoshiya12x <hikowaiide@gmail.com>
2019-06-03 22:52:59 +0900

    add ArticleRepositoryTest using mockwebserver

---
+++ app/src/test/java/e/yoppie/newengineerblogs/extension/MockWebServerExtension.kt
@@ -0,0 +1,18 @@
+@file:Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+package e.yoppie.newengineerblogs.extension
+
+import okhttp3.mockwebserver.MockResponse
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+
+fun MockResponse.setBodyFromFileName(name: String): MockResponse{
+    val inputStream = javaClass.classLoader.getResourceAsStream(name)
+    val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+    val stringBuilder = StringBuilder()
+    bufferedReader.forEachLine { buffer -> stringBuilder.append(buffer) }
+
+    val body = stringBuilder.toString()
+    this.setBody(body)
+    return this
+}
--- app/src/test/java/e/yoppie/newengineerblogs/repository/ArticleRepositoryTest.kt
+++ app/src/test/java/e/yoppie/newengineerblogs/repository/ArticleRepositoryTest.kt
@@ -1,25 +1,75 @@
 package e.yoppie.newengineerblogs.repository
 
+import e.yoppie.newengineerblogs.extension.setBodyFromFileName
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
 import org.junit.Test
 import org.assertj.core.api.Assertions.*
+import org.junit.After
 import org.junit.Before
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class ArticleRepositoryTest {
-    private lateinit var articleRepository: ArticleRepository
+    private val articleRepository = ArticleRepository()
+    private val mockWebServer = MockWebServer()
 
     @Before
     fun setUp() {
-        articleRepository = ArticleRepository()
+        val dispatcher: Dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest?): MockResponse {
+                return when{
+                    request!!.requestLine == "https://9hqe5z0uw7.execute-api.ap-northeast-1.amazonaws.com/test/articleList?userId=111"
+                    -> MockResponse().setResponseCode(200).setBodyFromFileName("articleList.json")
+                    request.requestLine == "https://9hqe5z0uw7.execute-api.ap-northeast-1.amazonaws.com/test/articleList?companyId=003&offset=1&userId=1"
+                    -> MockResponse().setResponseCode(200).setBodyFromFileName("articleListCompany.json")
+                    else
+                    -> MockResponse().setResponseCode(400)
+                }
+            }
+        }
+        mockWebServer.dispatcher = dispatcher
+        mockWebServer.start()
     }
 
     @Test
     fun getAllCategoryArticles() {
+        val categories = articleRepository.getAllCategoryArticles("111")
+                .test()
+                .await()
+                .assertNoErrors()
+                .assertComplete()
+                .values()[0]
+                .categories
+        assertThat(categories).isNotEmpty
+
+        articleRepository.getAllCategoryArticles("")
+                .test()
+                .await()
+                .assertNotComplete()
     }
 
     @Test
     fun getCategoryArticles() {
+        val categories = articleRepository.getCategoryArticles("003", "1", "1")
+                .test()
+                .await()
+                .assertNoErrors()
+                .assertComplete()
+                .values()[0]
+        assertThat(categories.articles).isNotEmpty
+
+        articleRepository.getCategoryArticles("003", "1", "")
+                .test()
+                .await()
+                .assertNotComplete()
+    }
+
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
     }
 }
